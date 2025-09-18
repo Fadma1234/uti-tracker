5493cbf86a7564a5ac26f47791089f24558d76ca
Fadma1234 <fadma.belkhouraf@gmail.com>
2025-09-17 22:32:06 -0400

    initial commit

---
+++ main.js
@@ -0,0 +1,39 @@
+document.getElementById('purple').onclick = partyPurple
+
+
+function partyPurple() {
+  document.querySelector('body').style.backgroundColor = 'rgb(237, 232, 191)'
+  document.querySelector('body').style.color = 'black'
+}
+
+
+document.getElementById('green').onclick = partyGreen
+
+
+function partyGreen() {
+  document.querySelector('body').style.backgroundColor = 'rgb(226, 201, 89)'
+  document.querySelector('body').style.color = 'black'
+}
+
+document.getElementById('blue').onclick = partyBlue
+
+
+function partyBlue() {
+  document.querySelector('body').style.backgroundColor = 'rgb(214, 165, 118)'
+  document.querySelector('body').style.color = 'black'
+}
+
+document.getElementById('pink').onclick = partyPink
+
+
+function partyPink() {
+  document.querySelector('body').style.backgroundColor = 'rgb(201, 125, 159)'
+  document.querySelector('body').style.color = 'black'
+}
+
+document.getElementById('red').onclick = partyRed
+
+function partyRed() {
+  document.querySelector('body').style.backgroundColor = 'rgb(128, 27, 91)'
+  document.querySelector('body').style.color = 'black'
+}
